61ed07349f49023f792e819582d49e5f33dbbbdb
shubham shahane <shubhamshahane41@gmail..com>
2025-05-13 17:51:36 +0530

    refactored

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 const Footer = () => {
+  let date = new Date();
+  const year = date.getFullYear();
   return (
     <footer class="font-lexend py-4 w-full min-h-full bg-gray-200">
       <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xxl md:px-24 lg:px-8">
         <div className="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
           <p className="text-sm text-gray-600">
-            © Copyright 2020 Lorem Inc. All rights reserved.
+            © Copyright {year} Shubham Shahane. All rights reserved.
           </p>
           <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
             <li>
